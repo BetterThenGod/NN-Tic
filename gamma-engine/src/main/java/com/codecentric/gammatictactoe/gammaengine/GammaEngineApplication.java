30b33f0b650316137a55506435b3be551c0c6f11
Thomas Jaspers <thomas.jaspers@codecentric.de>
2017-12-08 07:52:45 +0100

    Setting up the initial project from spring initializer

---
+++ gamma-engine/src/main/java/com/codecentric/gammatictactoe/gammaengine/GammaEngineApplication.java
@@ -0,0 +1,12 @@
+package com.codecentric.gammatictactoe.gammaengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GammaEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GammaEngineApplication.class, args);
+	}
+}
---
+++ gamma-engine/src/test/java/com/codecentric/gammatictactoe/gammaengine/GammaEngineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codecentric.gammatictactoe.gammaengine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GammaEngineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
